deaecc87f2c1044c62114f3cbb7fe01882e8d381
Dakota Clark <bannaray95@gmail.com>
2019-03-25 03:36:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,6 +11,10 @@ class App < Sinatra::Base
     @name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     
     erb :team
   end
